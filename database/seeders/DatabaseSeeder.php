ed8a2bc94ca9283e193ab0c656e7e12fe40c16c3
david <davidsanchezdiaz72@gmail.com>
2024-05-18 12:34:13 +0200

    Agrega roles y usuarios iniciales al seeder de la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Crear roles en la base de datos
+        Role::create(['name' => 'admin']); // Rol de administrador
+        Role::create(['name' => 'staff']); // Rol de personal
+        Role::create(['name' => 'client']); // Rol de cliente
+
+        // Crear usuarios con roles asignados
+        $users = \App\Models\User::factory(10)->create(); // Crear 10 usuarios ficticios
+
+        // Asignar el rol 'client' a los usuarios creados
+        foreach ($users as $user) {
+            $user->assignRole('client');
+        }
+
+        // Crear usuarios específicos y asignarles roles
+
+        // Crear usuario 'David' y asignarle el rol de administrador
+        $userAdmin = \App\Models\User::factory()->create([
+            'name' => 'David',
+            'email' => 'david@gmail.com'
+        ]); 
+        // Crear usuario 'Judy' y asignarle el rol de personal
+        $staff = \App\Models\User::factory()->create([
+            'name' => 'Judy',
+            'email' => 'judy@gmail.com'
+        ]); 
     }
 }
