163605eecd74df035f4a7e57eb3e57e29054cca7
david <davidsanchezdiaz72@gmail.com>
2024-05-24 11:54:49 +0200

    Agrupar rutas de autoeizacion

--- routes/web.php
+++ routes/web.php
@@ -6,36 +6,24 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MyScheduleController;
 
-
-
-// Define una ruta GET para la URL raíz ('/')
-// Esta ruta devuelve la vista 'welcome'
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Define una ruta de vista para la URL '/about'
-// Esta ruta simplemente devuelve la vista 'about'
 Route::view('/about', 'about');
 
-// Define una ruta GET para la URL '/dashboard'
-// Esta ruta devuelve la vista 'dashboard'
-// Aplica los middleware 'auth' y 'verified' para asegurar que el usuario esté autenticado y verificado
-// Además, nombra esta ruta como 'dashboard'
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-// Define una ruta GET para la URL '/my-schedule'
-// Esta ruta devuelve la vista 'my-schedule.index'
-// Aplica los middleware 'auth' y 'verified' para asegurar que el usuario esté autenticado y verificado
-// Además, nombra esta ruta como 'my-schedule'
-Route::get('/my-schedule', function () {
-    return view('my-schedule.index');
-})->middleware(['auth', 'verified'])->name('my-schedule');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
+    Route::get('/my-schedule', function () {
+        return view('my-schedule.index');
+    })->name('my-schedule');
 
-Route::get('/my-schedule', [MyScheduleController::class, 'index'])->name('my-schedule.index');
+    Route::get('/my-schedule', [MyScheduleController::class, 'index'])->name('my-schedule.index');
+});
 
 
 // Rutas protegidas por autenticación
