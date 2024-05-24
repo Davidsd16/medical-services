64d8b36407e28a84ec19da7725a46bd9d2c58807
david <davidsanchezdiaz72@gmail.com>
2024-05-24 19:12:41 +0200

    Crear formulario de reservas citas, componentes adicionales

--- resources/views/my-schedule/create.blade.php
+++ resources/views/my-schedule/create.blade.php
@@ -10,9 +10,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form>
+                        <div class="grid grid-cols-2 gap-4">
+                            <div>
+                                <x-label for="from[date]" :value="__('Fecha para la cita')" />
+                                <x-input id="from[date]" class="block mt-1 w-full" type="date" name="from[date]" :value="old('from.date', request('date'))" autofocus />
+                            </div>
+                            <div>
+                                <x-label for="from[time]" :value="__('Elije la hora de inicio')" />
+                                <x-select-time id="from[time]" init-hour="8" end-hour="17" :selected-hour="old('from.time')" class="block mt-1 w-full" name="from[time]"></x-select-time>
+                            </div>                        
+                            <div>
+                                <x-label for="staff_user_id" :value="__('Elige quién te atenderá')" />
+                                    <x-select id="staff_user_id" class="block mt-1 w-full" name="staff_user_id">
+                                            <option></option>
+                                    </x-select>
+                            </div>
+                        </div>
+                        <x-button class="mt-4">Reservar</x-button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>    
-
 </x-app-layout>
