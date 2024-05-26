518332e0a76747f7fccd341ef0cce219c0445edc
david <davidsanchezdiaz72@gmail.com>
2024-05-26 19:27:06 +0200

    Añadir comentarios en StaffAvailabilityChecker

--- app/Business/StaffAvailabilityChecker.php
+++ app/Business/StaffAvailabilityChecker.php
@@ -8,10 +8,18 @@ use App\Models\Scheduler;
 
 class StaffAvailabilityChecker
 {
+    // Propiedades protegidas para el usuario del personal y los tiempos de inicio y fin de la cita
     protected $staffUser;
     protected $from;
     protected $to;
 
+    /**
+     * Constructor para inicializar las propiedades del verificador de disponibilidad del personal.
+     *
+     * @param User $staffUser El usuario del personal
+     * @param Carbon $from La fecha y hora de inicio de la cita
+     * @param Carbon $to La fecha y hora de fin de la cita
+     */
     public function __construct(User $staffUser, Carbon $from, Carbon $to)
     {
         $this->staffUser = $staffUser;
@@ -19,12 +27,17 @@ class StaffAvailabilityChecker
         $this->to = $to;
     }
 
+    /**
+     * Verifica la disponibilidad del personal para la cita especificada.
+     *
+     * @return bool True si el personal está disponible, False si no lo está
+     */
     public function check()
     {
-        return ! Scheduler::where('staff_user_id', $this->staffUser->id)
-            ->where('from', '<', $this->to)
-            ->where('to', '>', $this->from)
-            ->exists();
+        // Busca en la tabla 'scheduler' si hay algún conflicto de horario para el personal
+        return !Scheduler::where('staff_user_id', $this->staffUser->id)
+            ->where('from', '<', $this->to) // La cita comienza antes de que termine la nueva cita
+            ->where('to', '>', $this->from) // La cita termina después de que comience la nueva cita
+            ->exists(); // Verifica si existe algún registro que coincida con los criterios anteriores
     }
-
 }
